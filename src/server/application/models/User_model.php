118964b45d1f1f20ca819f9bb1caac339c1e7eee
SJ19565 <1956584181@qq.com>
2018-06-02 19:33:38 +0800

    11

--- src/server/application/models/User_model.php
+++ src/server/application/models/User_model.php
@@ -10,6 +10,15 @@ class User_model extends CI_Model{
 	 		"glod"=>$glod;
 	 	))->row();
 	 } 
-	 
+
+	 //////////签到/////////////////
+	  public function update_sign_in_($uid){
+		$data=array(
+    		'mark'=>++$mark,
+    		'leaves'=>$leaves+=15,
+		);
+		$bool=$this->db->update('user',$data,array('openid'=>$uid));//将数据库.user表里id=3的用户密码给为12345
+		return $bool;
+	  }
 	 
 }
