2c1b4ea2267c6dea4e70cb2567fb27a6f6b8d712
yuanqinglong <yuanql96@yeah.net>
2018-06-06 17:38:03 +0800

    修改错误

--- src/server/application/models/Vocabulary_model.php
+++ src/server/application/models/Vocabulary_model.php
@@ -8,17 +8,27 @@ class Vocabulary_model extends CI_Model
 
   // 获取单词id，词汇文本和正确、错误选项
   public function get_answer_by_vocabulary(){
+    	$pdo = DB::getInstance();
       $sql = 'SELECT  * FROM `word`
               WHERE (word_id >= ((SELECT MAX(word_id) FROM `word`)-(SELECT MIN(word_id) FROM `word`)) * RAND() 
                 + (SELECT MIN(word_id) FROM `word`) )LIMIT 12' ;          
-      $query = $this->db->query($sql);
-      return $query->result();
+      $query = $pdo->query($sql);
+      if($res != 'FALSE'){
+        $data = [];
+        while($row = $res->fetch(PDO::FETCH_ASSOC)){
+          $data[] = $row;
+        }
+			  return $data;
+		  }else{
+		  	return 'FALSE';
+		  }
   }
   
   //更新正确的单词数量
+  // return  受影响行数
   public function update_word($u_id,$words){
     $res = DB::update('user ', ['words' => $words], ['u_id' => $u_id]);
-    return $this->db->affected_rows();
+    return $res;
   }
 
   //by 袁庆龙 end
